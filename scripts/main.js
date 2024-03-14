04f2c0fec0008e28b36a3b80daebf54f7ee1674d
mcheich <mcheich@gmail.com>
2024-03-14 18:57:54 -0400

    update: finally made i click

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let score = 0;
+
+const clickerButton = document.getElementById("clickerbutton");
+
+
+
+clickerButton.addEventListener("click", function () {
+    score++;
+    document.getElementById("score").innerHTML = score;
+    console.log(score);
+});
+
+// const element = document.getElementById("clickerbutton");
+// element.addEventListener("click", function () {
+//     document.getElementById("myBtn").innerHTML = "Hello World";
+// });
